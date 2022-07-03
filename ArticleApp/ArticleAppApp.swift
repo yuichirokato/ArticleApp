6df162cf5183c27d159375f9e02f85964d6c6578
y.takahashi <ikmy0709@gmail.com>
2022-07-03 20:28:52 +0900

    Initial Commit

---
+++ ArticleApp/ArticleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleAppApp.swift
+//  ArticleApp
+//
+//  Created by Yuichiro Takahashi on 2022/07/03.
+//
+
+import SwiftUI
+
+@main
+struct ArticleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArticleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArticleApp
+//
+//  Created by Yuichiro Takahashi on 2022/07/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
